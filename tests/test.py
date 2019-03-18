0c4f05ce171604816e8fb6950b9a2df02170e069
Oliver Broadhurst <Oliver@explore-datascience.net>
2019-03-18 16:15:14 +0200

    Some fixes

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from mypackage import myModule
+from . import myModule
 
 def test_top_n():
     """
@@ -9,4 +9,4 @@ def test_top_n():
     assert myModule.top_n([10, 1, 12, 9, 2], 2) == [12, 10], 'incorrect'
     assert myModule.top_n([1, 2, 3, 4, 5], 5) == [5, 4, 3, 2, 1], 'incorrect'
 
-#print(test_top_n())
+print(test_top_n())
